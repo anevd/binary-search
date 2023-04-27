c1d9f8c7694d8f1cb024d972bbf365a25025577d
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-04-27 20:32:27 +0300

    add solution

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
-
+function binarySearch(n, arr, start, end) {
+	let middle = Math.floor((start + end) / 2);
+	if (arr[middle] === n) {
+		return middle;
+	}
+	if (arr[middle] > n) {
+		return binarySearch(n, arr, start, middle - 1);
+	}
+	if (arr[middle] < n) {
+		return binarySearch(n, arr, middle + 1, end);
+	}
+	return -1;
 }
 
-module.exports = binarySearch
+module.exports = binarySearch;
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,27 @@
-const binarySearch = require('../index')
+const binarySearch = require("../index");
 
-test('Функция должна возвращать индекс элемента', () => {
-    
-})
+test("Функция должна возвращать индекс элемента", () => {
+	let n = "G";
+	let arr = ["A", "B", "C", "D", "E", "F", "G", "H"];
+	let start = 0;
+	let end = arr.length;
+	expect(binarySearch(n, arr, start, end)).toEqual(6);
+	expect(binarySearch(n, arr, start, end)).toBe(6);
+});
 
-test('Функция должна быть определена', () => {
-    
-})
+test("Функция должна быть определена", () => {
+	let n = "G";
+	let arr = ["A", "B", "C", "D", "E", "F", "G", "H"];
+	let start = 0;
+	let end = arr.length;
+	expect(binarySearch(n, arr, start, end)).toBeDefined();
+});
 
-test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
-})
+test("Функция должна возвращать -1, если элемента не существует в данном массиве", () => {
+	let n = "K";
+	let arr = ["A", "B", "C", "D", "E", "F", "G", "H"];
+	let start = 0;
+	let end = arr.length;
+	expect(binarySearch(n, arr, start, end)).toEqual(-1);
+	expect(binarySearch(n, arr, start, end)).toBe(-1);
+});
